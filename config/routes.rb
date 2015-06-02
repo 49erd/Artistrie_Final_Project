90184601336d3e071a91907af578c92dda569979
Jeffrey Ercolani <Jercolani@JeffyMBP.local>
2015-06-02 15:49:37 -0700

    switched to postgresql database

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  get '/' => 'default#index'
+  root to: 'default#index'
   get '/contact', to: 'default#contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
