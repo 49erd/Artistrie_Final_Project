7455d5f578d33cb17a4c36ff6f56db2973c9b9b7
ChhunSo <chhunso@Elizabeths-MacBook-Air-2.local>
2015-06-03 13:39:08 -0700

    initial profile page functionality

--- app/assets/javascripts/angular-app/controllers/ExampleCtrl.js
+++ app/assets/javascripts/angular-app/controllers/ExampleCtrl.js
@@ -1,5 +1,33 @@
-angular.module('ArtistrieApp').controller("ExampleCtrl", [
-  '$scope', function($scope){
+angular.module('ArtistrieApp')
+
+.service('Pieces',function(){
+	$scope.Pieces= Pieces.all();
+
+    	$scope.Pieces = $http.get('/pieces')
+    	.success(function(data){
+    		console.log(data);
+    		$scope.Pieces= data;
+    	})
+    	.error(function(data){
+    		console.log("error:" + data);
+    	})
+})
+.controller("ExampleCtrl", [
+  '$scope', "Pieces", function($scope,Pieces){
     console.log('ExampleCtrl running');
     $scope.exampleValue = "Hello angular and rails";
+    
 }])
+
+.controller('User',['$scope',function($scope){
+	$scope.posts = [
+	{
+		title:'I Get Out',
+		artist:'Marley'
+	},
+	{
+		title: 'Hi',
+		artist: 'Rico'
+	}
+	]
+}]);
